9d9d14d1f87e051553e8edebe8ab4783ac1b268c
john <lord1229@gmail.com>
2020-05-20 01:16:37 +0900

    header 없앰.

--- App.js
+++ App.js
@@ -19,7 +19,11 @@ export default function App(props) {
       <View style={styles.container}>
         {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
         <NavigationContainer linking={LinkingConfiguration}>
-          <Stack.Navigator>
+          <Stack.Navigator 
+            screenOptions={{
+              headerShown: false
+            }}
+          >
             <Stack.Screen name="Root" component={BottomTabNavigator} />
           </Stack.Navigator>
         </NavigationContainer>
