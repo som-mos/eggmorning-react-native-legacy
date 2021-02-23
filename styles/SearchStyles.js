f45feb3561e3e04dd7f48346c0b261c7881269b9
lsk3936 <asha@ntcon.co>
2021-02-23 13:07:04 +0900

    web에서 안보이던 native-base tab 수정. 아직도 오류는 뜨지만 웹에서 볼 수 있음.

--- styles/SearchStyles.js
+++ styles/SearchStyles.js
@@ -48,26 +48,6 @@ const Searchstyles = StyleSheet.create({
       lineHeight: 24,
       textAlign: 'center',
     },
-    tabBarInfoContainer: {
-      position: 'absolute',
-      bottom: 0,
-      left: 0,
-      right: 0,
-      ...Platform.select({
-        ios: {
-          shadowColor: 'black',
-          shadowOffset: { width: 0, height: -3 },
-          shadowOpacity: 0.1,
-          shadowRadius: 3,
-        },
-        android: {
-          elevation: 20,
-        },
-      }),
-      alignItems: 'center',
-      backgroundColor: '#fbfbfb',
-      paddingVertical: 20,
-    },
     tabBarInfoText: {
       fontSize: 17,
       color: 'rgba(96,100,109, 1)',
